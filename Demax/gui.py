8f91b4e2ba9fc99205b94b8ea76f57298fcd8862
nano-rex <buildblock@protonmail.com>
2023-07-29 14:26:30 -0400

    aider: Reverted back to remove the default directory setting in the file dialog.
    # Aider chat conversation:
    
    # Context:
    USER: revert back

--- Demax/gui.py
+++ Demax/gui.py
@@ -1,4 +1,3 @@
 def open_folder():
     global filepath1
-    initialdir = "/path/to/default/directory"  # Set the default directory here
-    filepath1 = filedialog.askopenfilename(initialdir=initialdir, filetypes=[("Excel Files", "*.xlsx")])
+    filepath1 = filedialog.askopenfilename(filetypes=[("Excel Files", "*.xlsx")])
