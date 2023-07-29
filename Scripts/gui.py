122a8edbacb9d771931878922e1d95ff4817ecb6
nano-rex <buildblock@protonmail.com>
2023-07-29 13:16:40 -0400

    aider: Add functionality to select an .xlsx file, add "testing" to the bottom line, and save it as a new file with a prefix "INV_".
    # Aider chat conversation:
    
    # Context:
    USER: is there a common error that pandas cannot work inside tkinter?
    USER: ok, let's start from scratch. You can erase this code completely and start new.
    USER: Create a python code that pop out a gui interface with a button, when clicking the button, it opens the file directory for user to choose a .xlsx file. After the .xlsx file is selected, add "testing" to the bottom line inside the xlsx file as a new file named INV_[the original filename].xlsx in the same path as the original .xlsx file

--- Scripts/gui.py
+++ Scripts/gui.py
@@ -1,60 +1,38 @@
-#!/bin/python3
-
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
+from tkinter import filedialog
 import pandas as pd
-import openpyxl
-import shutil
+import os
 
-# Root window
-root = tk.Tk()
-root.title('Display a Text File')
-root.resizable(False, False)
-root.geometry('550x250')
-
-# Text editor
-text = tk.Text(root, height=12)
-text.grid(column=0, row=0, sticky='nsew')
-
-def open_text_file():
-    # file type
-    filetypes = (
-        ('Excel files', '*.xlsx'),
-        ('All files', '*.*')
-    )
-    # show the open file dialog
-    filepath = fd.askopenfilename(filetypes=filetypes)
+def add_text_to_xlsx():
+    # Open file dialog to select .xlsx file
+    file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
     
-    if filepath:
+    if file_path:
+        # Read the original file
+        df = pd.read_excel(file_path)
+        
+        # Add "testing" to the bottom line
+        df.loc[len(df)] = ["testing"]
+        
         # Create the new file path with "INV_" as the prefix
-        def add_prefix_to_last_word(text, prefix):
-           words = text.split("/") # Split the text by forward slashes
-           last_word = words[-1] # Get the last word after the last forward slash
-           new_last_word = prefix + last_word # Add the prefix to the last word
-           words[-1] = new_last_word # Replace the last word with the updated one 
-           modified_text = "/".join(words) # Join the words back together with forward slashes
-           return modified_text
-        text = filepath
-        prefix = "INV_"
-        new_filepath = add_prefix_to_last_word(filepath, prefix)
-        shutil.copyfile(filepath, new_filepath)
+        file_dir, file_name = os.path.split(file_path)
+        new_file_name = f"INV_{file_name}"
+        new_file_path = os.path.join(file_dir, new_file_name)
         
-        # Update the text widget with the new file path
-        text.delete('1.0', tk.END)
-        text.insert(tk.END, new_filepath)
+        # Save the modified dataframe as a new .xlsx file
+        df.to_excel(new_file_path, index=False)
         
-        # Call the xlsx() function to perform the desired operations
-        xlsx()
+        # Show a message box with the new file path
+        tk.messagebox.showinfo("File Saved", f"The modified file has been saved as:\n{new_file_path}")
 
-# open file button
-open_button = ttk.Button(
-    root,
-    text='Open a File',
-    command=open_text_file
-)
+# Create the GUI window
+window = tk.Tk()
+window.title("Add Text to XLSX")
+window.geometry("300x100")
 
-open_button.grid(column=0, row=1, sticky='w', padx=10, pady=10)
+# Create the button
+button = tk.Button(window, text="Select XLSX File", command=add_text_to_xlsx)
+button.pack(pady=20)
 
-# run the application
-root.mainloop()
+# Run the GUI window
+window.mainloop()
