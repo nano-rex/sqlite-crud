4074c0b32435893c0869aa0a2d868e25940d841c
nano-rex <buildblock@protonmail.com>
2023-07-29 16:43:56 -0400

    aider: work in progress
    # Aider chat conversation:
    
    # Context:
    USER: convert this code into java
    USER: this is not your previous commit

--- git/sqlite-crud/crud.java
+++ git/sqlite-crud/crud.java
@@ -1,150 +1,175 @@
-#!/bin/python3
+import java.sql.*;
 
-import sqlite3
+public class CRUD {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:data.db");
+            System.out.println("Opened database successfully");
 
-####################################################
-def create():
-	conn = sqlite3.connect('data.db')
-	print("Opened database successfully");
-	conn.execute('''
-		CREATE TABLE Users
-		(ID      INT   PRIMARY KEY  NOT NULL,
-		 NAME    TEXT               NOT NULL,
-		 AGE     INT                NOT NULL,
-		 GENDER  TEXT,
-		 SALARY  INT);
-	''')
-	print("Table created successfully");
+            Statement stmt = conn.createStatement();
+            String createTableQuery = "CREATE TABLE IF NOT EXISTS Users " +
+                    "(ID INT PRIMARY KEY NOT NULL, " +
+                    "NAME TEXT NOT NULL, " +
+                    "AGE INT NOT NULL, " +
+                    "GENDER TEXT, " +
+                    "SALARY INT)";
+            stmt.executeUpdate(createTableQuery);
+            System.out.println("Table created successfully");
 
-####################################################
-# FOR CREATING RECORDS FUNCTION DEFINITION
-def insert():
-#	try:
-		con = sqlite3.connect("data.db")
-		cursor = con.cursor()
-		while (True):
-			idd = int(input("Enter ID: "))
-			name = input("Enter Name: ")
-			age = int(input("Enter Age: "))
-			gender = input("Enter your Gender: ")
-			salary = int(input("Enter your Salary: "))
-			query = "INSERT into USERS(ID,NAME,AGE,GENDER,SALARY) VALUES (?,?,?,?,?);"
-			data = (idd,name,age,gender,salary)
-			cursor.execute(query, data)
-			con.commit()
-			if(cursor.execute(query,data)):
-				print("Data Inserted Successfully")
-			else:
-				print("Data not Inserted")
-#			ch = input("Do You want to Add More Records(Y/N): ")
-#			if ch == "N" or ch == "n":
-#				cursor.close()
-#				break
-#			else:
-#				pass
-#	except:
-#		print("Error in Record Creation\n")
+            while (true) {
+                System.out.println("1). Insert Records");
+                System.out.println("2). Read Records");
+                System.out.println("3). Update Records");
+                System.out.println("4). Delete Records");
+                System.out.println("5). Exit");
+                int choice = Integer.parseInt(System.console().readLine("Enter Your Choice: "));
 
-####################################################
-# FOR READING ONE RECORD FUNCTION DEFINITION
-def read_one():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    ids = int(input("Enter Your ID: "))
-    query = "SELECT * from USERS WHERE id = ?"
-    result = cursor.execute(query, (ids,))
-    if (result):
-        for i in result:
-            print(f"Name is: {i[1]}")
-            print(f"Age is: {i[2]}")
-            print(f"Salary is: {i[4]}")
-    else:
-        print("Roll Number Does not Exist")
-        cursor.close()
+                if (choice == 1) {
+                    insert(conn);
+                } else if (choice == 2) {
+                    System.out.println("1). Read Single Record");
+                    System.out.println("2). Read All Records");
+                    int readChoice = Integer.parseInt(System.console().readLine("Enter Your Choice: "));
+                    if (readChoice == 1) {
+                        readOne(conn);
+                    } else if (readChoice == 2) {
+                        readAll(conn);
+                    } else {
+                        System.out.println("Wrong Choice Entered");
+                    }
+                } else if (choice == 3) {
+                    update(conn);
+                } else if (choice == 4) {
+                    delete(conn);
+                } else if (choice == 5) {
+                    break;
+                } else {
+                    System.out.println("Enter Correct Choice");
+                }
+            }
 
-####################################################
-# FOR READING ALL RECORDS FUNCTION DEFINITION
-def read_all():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    query = "SELECT * from USERS"
-    result = cursor.execute(query)
-    if (result):
-        print("\n&lt;===Available Records===&gt;")
-        for i in result:
-            print(f"Name is : {i[1]}")
-            print(f"Age is : {i[2]}")
-            print(f"Salary is : {i[4]}\n")
-    else:
-        pass
-    
-####################################################
-# FOR UPDATING RECORDS FUNCTION DEFINITION
-def update():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    idd = int(input("Enter ID: "))
-    name = input("Enter Name: ")
-    age = int(input("Enter Age: "))
-    gender = input("Enter Gender: ")
-    salary = int(input("Enter Salary: "))
-    data = (name, age, gender, salary, idd,)
-    query = "UPDATE USERS set name = ?, age = ?, gender = ?, salary = ? WHERE id = ?"
-    result = cursor.execute(query, data)
-    con.commit()
-    cursor.close()
-    if (result):
-        print("Records Updated")
-    else:
-        print("Something Error in Updation")
+            conn.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
 
-####################################################
-# FOR DELETING RECORDS FUNCTION DEFINITION
-def delete():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    idd = int(input("Enter ID: "))
-    query = "DELETE from USERS where ID = ?"
-    result = cursor.execute(query, (idd,))
-    con.commit()
-    cursor.close()
-    if (result):
-        print("One record Deleted")
-    else:
-        print("Something Error in Deletion")
+    public static void insert(Connection conn) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO Users (ID, NAME, AGE, GENDER, SALARY) VALUES (?, ?, ?, ?, ?)");
 
-####################################################
-# MAIN BLOCK
-try:
-    while (True):
-        print("1). Insert Records: ")
-        print("2). Read Records: ")
-        print("3). Update Records: ")
-        print("4). Delete Records: ")
-        print("5). Exit")
-        ch = int(input("Enter Your Choice: "))
-        if (ch == 1):
-            insert()
-        elif (ch == 2):
-            print("1). Read Single Record")
-            print("2). Read All Records")
-            choice = int(input("Enter Your Choice: "))
-            if (choice == 1):
-                read_one()
-            elif (choice == 2):
-                read_all()
-            else:
-                print("Wrong Choice Entered")
-        elif (ch == 3):
-            update()
-        elif (ch == 4):
-            delete()
-        elif (ch == 5):
-            break
-        else:
-            print("Enter Correct Choice")
-except:
-    print("Database Error")
+            int id = Integer.parseInt(System.console().readLine("Enter ID: "));
+            String name = System.console().readLine("Enter Name: ");
+            int age = Integer.parseInt(System.console().readLine("Enter Age: "));
+            String gender = System.console().readLine("Enter Gender: ");
+            int salary = Integer.parseInt(System.console().readLine("Enter Salary: "));
 
-####################################################
-#END
+            pstmt.setInt(1, id);
+            pstmt.setString(2, name);
+            pstmt.setInt(3, age);
+            pstmt.setString(4, gender);
+            pstmt.setInt(5, salary);
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Data Inserted Successfully");
+            } else {
+                System.out.println("Data not Inserted");
+            }
+
+            pstmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public static void readOne(Connection conn) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM Users WHERE ID = ?");
+            int id = Integer.parseInt(System.console().readLine("Enter Your ID: "));
+            pstmt.setInt(1, id);
+
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                System.out.println("Name is: " + rs.getString("NAME"));
+                System.out.println("Age is: " + rs.getInt("AGE"));
+                System.out.println("Salary is: " + rs.getInt("SALARY"));
+            } else {
+                System.out.println("ID Does not Exist");
+            }
+
+            rs.close();
+            pstmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public static void readAll(Connection conn) {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Users");
+
+            System.out.println("\n<=== Available Records ===>");
+            while (rs.next()) {
+                System.out.println("Name is: " + rs.getString("NAME"));
+                System.out.println("Age is: " + rs.getInt("AGE"));
+                System.out.println("Salary is: " + rs.getInt("SALARY") + "\n");
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public static void update(Connection conn) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("UPDATE Users SET NAME = ?, AGE = ?, GENDER = ?, SALARY = ? WHERE ID = ?");
+
+            int id = Integer.parseInt(System.console().readLine("Enter ID: "));
+            String name = System.console().readLine("Enter Name: ");
+            int age = Integer.parseInt(System.console().readLine("Enter Age: "));
+            String gender = System.console().readLine("Enter Gender: ");
+            int salary = Integer.parseInt(System.console().readLine("Enter Salary: "));
+
+            pstmt.setString(1, name);
+            pstmt.setInt(2, age);
+            pstmt.setString(3, gender);
+            pstmt.setInt(4, salary);
+            pstmt.setInt(5, id);
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Records Updated");
+            } else {
+                System.out.println("Something Error in Updation");
+            }
+
+            pstmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public static void delete(Connection conn) {
+        try {
+            PreparedStatement pstmt = conn.prepareStatement("DELETE FROM Users WHERE ID = ?");
+
+            int id = Integer.parseInt(System.console().readLine("Enter ID: "));
+            pstmt.setInt(1, id);
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("One record Deleted");
+            } else {
+                System.out.println("Something Error in Deletion");
+            }
+
+            pstmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+}
