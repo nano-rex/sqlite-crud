5f8a826219a7106058d012db6b102d842893d7a0
nano-rex <buildblock@protonmail.com>
2023-07-29 16:51:11 -0400

    aider: work in progress
    # Aider chat conversation:
    
    # Context:
    USER: convert this c++ code into c code

--- git/sqlite-crud/crud.c
+++ git/sqlite-crud/crud.c
@@ -1,23 +1,23 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sqlite3.h>
-
-using namespace std;
 
 void create() {
     sqlite3 *db;
     int rc = sqlite3_open("data.db", &db);
     if (rc) {
-        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        printf("Can't open database: %s\n", sqlite3_errmsg(db));
         return;
     }
-    cout << "Opened database successfully" << endl;
+    printf("Opened database successfully\n");
     
     const char *sql = "CREATE TABLE Users (ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, AGE INT NOT NULL, GENDER TEXT, SALARY INT);";
     rc = sqlite3_exec(db, sql, 0, 0, 0);
     if (rc != SQLITE_OK) {
-        cout << "SQL error: " << sqlite3_errmsg(db) << endl;
+        printf("SQL error: %s\n", sqlite3_errmsg(db));
     } else {
-        cout << "Table created successfully" << endl;
+        printf("Table created successfully\n");
     }
     
     sqlite3_close(db);
@@ -27,7 +27,7 @@ void insert() {
     sqlite3 *db;
     int rc = sqlite3_open("data.db", &db);
     if (rc) {
-        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        printf("Can't open database: %s\n", sqlite3_errmsg(db));
         return;
     }
     
@@ -35,43 +35,43 @@ void insert() {
     const char *sql = "INSERT INTO USERS (ID, NAME, AGE, GENDER, SALARY) VALUES (?, ?, ?, ?, ?);";
     rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     if (rc != SQLITE_OK) {
-        cout << "SQL error: " << sqlite3_errmsg(db) << endl;
+        printf("SQL error: %s\n", sqlite3_errmsg(db));
         return;
     }
     
-    while (true) {
+    while (1) {
         int idd, age, salary;
-        string name, gender;
+        char name[100], gender[10];
         
-        cout << "Enter ID: ";
-        cin >> idd;
-        cout << "Enter Name: ";
-        cin >> name;
-        cout << "Enter Age: ";
-        cin >> age;
-        cout << "Enter your Gender: ";
-        cin >> gender;
-        cout << "Enter your Salary: ";
-        cin >> salary;
+        printf("Enter ID: ");
+        scanf("%d", &idd);
+        printf("Enter Name: ");
+        scanf("%s", name);
+        printf("Enter Age: ");
+        scanf("%d", &age);
+        printf("Enter your Gender: ");
+        scanf("%s", gender);
+        printf("Enter your Salary: ");
+        scanf("%d", &salary);
         
         sqlite3_bind_int(stmt, 1, idd);
-        sqlite3_bind_text(stmt, 2, name.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 2, name, -1, SQLITE_STATIC);
         sqlite3_bind_int(stmt, 3, age);
-        sqlite3_bind_text(stmt, 4, gender.c_str(), -1, SQLITE_STATIC);
+        sqlite3_bind_text(stmt, 4, gender, -1, SQLITE_STATIC);
         sqlite3_bind_int(stmt, 5, salary);
         
         rc = sqlite3_step(stmt);
         if (rc != SQLITE_DONE) {
-            cout << "Data not Inserted" << endl;
+            printf("Data not Inserted\n");
         } else {
-            cout << "Data Inserted Successfully" << endl;
+            printf("Data Inserted Successfully\n");
         }
         
         sqlite3_reset(stmt);
         
         char ch;
-        cout << "Do You want to Add More Records(Y/N): ";
-        cin >> ch;
+        printf("Do You want to Add More Records(Y/N): ");
+        scanf(" %c", &ch);
         if (ch == 'N' || ch == 'n') {
             break;
         }
@@ -85,7 +85,7 @@ void read_one() {
     sqlite3 *db;
     int rc = sqlite3_open("data.db", &db);
     if (rc) {
-        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        printf("Can't open database: %s\n", sqlite3_errmsg(db));
         return;
     }
     
@@ -93,23 +93,23 @@ void read_one() {
     const char *sql = "SELECT * FROM USERS WHERE id = ?;";
     rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     if (rc != SQLITE_OK) {
-        cout << "SQL error: " << sqlite3_errmsg(db) << endl;
+        printf("SQL error: %s\n", sqlite3_errmsg(db));
         return;
     }
     
     int ids;
-    cout << "Enter Your ID: ";
-    cin >> ids;
+    printf("Enter Your ID: ");
+    scanf("%d", &ids);
     
     sqlite3_bind_int(stmt, 1, ids);
     
     rc = sqlite3_step(stmt);
     if (rc == SQLITE_ROW) {
-        cout << "Name is: " << sqlite3_column_text(stmt, 1) << endl;
-        cout << "Age is: " << sqlite3_column_int(stmt, 2) << endl;
-        cout << "Salary is: " << sqlite3_column_int(stmt, 4) << endl;
+        printf("Name is: %s\n", sqlite3_column_text(stmt, 1));
+        printf("Age is: %d\n", sqlite3_column_int(stmt, 2));
+        printf("Salary is: %d\n", sqlite3_column_int(stmt, 4));
     } else {
-        cout << "Record Does not Exist" << endl;
+        printf("Record Does not Exist\n");
     }
     
     sqlite3_finalize(stmt);
@@ -120,7 +120,7 @@ void read_all() {
     sqlite3 *db;
     int rc = sqlite3_open("data.db", &db);
     if (rc) {
-        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        printf("Can't open database: %s\n", sqlite3_errmsg(db));
         return;
     }
     
@@ -128,18 +128,18 @@ void read_all() {
     const char *sql = "SELECT * FROM USERS;";
     rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     if (rc != SQLITE_OK) {
-        cout << "SQL error: " << sqlite3_errmsg(db) << endl;
+        printf("SQL error: %s\n", sqlite3_errmsg(db));
         return;
     }
     
     rc = sqlite3_step(stmt);
     if (rc == SQLITE_ROW) {
-        cout << "\n<=== Available Records ===>" << endl;
+        printf("\n<=== Available Records ===>\n");
         while (rc == SQLITE_ROW) {
-            cout << "Name is: " << sqlite3_column_text(stmt, 1) << endl;
-            cout << "Age is: " << sqlite3_column_int(stmt, 2) << endl;
-            cout << "Salary is: " << sqlite3_column_int(stmt, 4) << endl;
-            cout << endl;
+            printf("Name is: %s\n", sqlite3_column_text(stmt, 1));
+            printf("Age is: %d\n", sqlite3_column_int(stmt, 2));
+            printf("Salary is: %d\n", sqlite3_column_int(stmt, 4));
+            printf("\n");
             rc = sqlite3_step(stmt);
         }
     }
@@ -152,7 +152,7 @@ void update() {
     sqlite3 *db;
     int rc = sqlite3_open("data.db", &db);
     if (rc) {
-        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        printf("Can't open database: %s\n", sqlite3_errmsg(db));
         return;
     }
     
@@ -160,35 +160,35 @@ void update() {
     const char *sql = "UPDATE USERS SET NAME = ?, AGE = ?, GENDER = ?, SALARY = ? WHERE ID = ?;";
     rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     if (rc != SQLITE_OK) {
-        cout << "SQL error: " << sqlite3_errmsg(db) << endl;
+        printf("SQL error: %s\n", sqlite3_errmsg(db));
         return;
     }
     
     int idd, age, salary;
-    string name, gender;
+    char name[100], gender[10];
     
-    cout << "Enter ID: ";
-    cin >> idd;
-    cout << "Enter Name: ";
-    cin >> name;
-    cout << "Enter Age: ";
-    cin >> age;
-    cout << "Enter Gender: ";
-    cin >> gender;
-    cout << "Enter Salary: ";
-    cin >> salary;
+    printf("Enter ID: ");
+    scanf("%d", &idd);
+    printf("Enter Name: ");
+    scanf("%s", name);
+    printf("Enter Age: ");
+    scanf("%d", &age);
+    printf("Enter Gender: ");
+    scanf("%s", gender);
+    printf("Enter Salary: ");
+    scanf("%d", &salary);
     
-    sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
     sqlite3_bind_int(stmt, 2, age);
-    sqlite3_bind_text(stmt, 3, gender.c_str(), -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, gender, -1, SQLITE_STATIC);
     sqlite3_bind_int(stmt, 4, salary);
     sqlite3_bind_int(stmt, 5, idd);
     
     rc = sqlite3_step(stmt);
     if (rc != SQLITE_DONE) {
-        cout << "Something Error in Updation" << endl;
+        printf("Something Error in Updation\n");
     } else {
-        cout << "Records Updated" << endl;
+        printf("Records Updated\n");
     }
     
     sqlite3_finalize(stmt);
@@ -199,7 +199,7 @@ void deleteRecord() {
     sqlite3 *db;
     int rc = sqlite3_open("data.db", &db);
     if (rc) {
-        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        printf("Can't open database: %s\n", sqlite3_errmsg(db));
         return;
     }
     
@@ -207,21 +207,21 @@ void deleteRecord() {
     const char *sql = "DELETE FROM USERS WHERE ID = ?;";
     rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
     if (rc != SQLITE_OK) {
-        cout << "SQL error: " << sqlite3_errmsg(db) << endl;
+        printf("SQL error: %s\n", sqlite3_errmsg(db));
         return;
     }
     
     int idd;
-    cout << "Enter ID: ";
-    cin >> idd;
+    printf("Enter ID: ");
+    scanf("%d", &idd);
     
     sqlite3_bind_int(stmt, 1, idd);
     
     rc = sqlite3_step(stmt);
     if (rc != SQLITE_DONE) {
-        cout << "Something Error in Deletion" << endl;
+        printf("Something Error in Deletion\n");
     } else {
-        cout << "One record Deleted" << endl;
+        printf("One record Deleted\n");
     }
     
     sqlite3_finalize(stmt);
@@ -229,35 +229,35 @@ void deleteRecord() {
 }
 
 int main() {
-    while (true) {
-        cout << "1). Insert Records" << endl;
-        cout << "2). Read Records" << endl;
-        cout << "3). Update Records" << endl;
-        cout << "4). Delete Records" << endl;
-        cout << "5). Exit" << endl;
-        cout << "Enter Your Choice: ";
+    while (1) {
+        printf("1). Insert Records\n");
+        printf("2). Read Records\n");
+        printf("3). Update Records\n");
+        printf("4). Delete Records\n");
+        printf("5). Exit\n");
+        printf("Enter Your Choice: ");
         
         int ch;
-        cin >> ch;
+        scanf("%d", &ch);
         
         switch (ch) {
             case 1:
                 insert();
                 break;
             case 2:
-                cout << "1). Read Single Record" << endl;
-                cout << "2). Read All Records" << endl;
-                cout << "Enter Your Choice: ";
+                printf("1). Read Single Record\n");
+                printf("2). Read All Records\n");
+                printf("Enter Your Choice: ");
                 
                 int choice;
-                cin >> choice;
+                scanf("%d", &choice);
                 
                 if (choice == 1) {
                     read_one();
                 } else if (choice == 2) {
                     read_all();
                 } else {
-                    cout << "Wrong Choice Entered" << endl;
+                    printf("Wrong Choice Entered\n");
                 }
                 
                 break;
@@ -270,7 +270,7 @@ int main() {
             case 5:
                 return 0;
             default:
-                cout << "Enter Correct Choice" << endl;
+                printf("Enter Correct Choice\n");
                 break;
         }
     }
