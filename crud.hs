ba92d8fbc0de35c72d369d88dc7e089c15c4ae22
nano-rex <buildblock@protonmail.com>
2023-07-29 18:44:17 -0400

    aider: Refactor CRUD operations into Haskell functions and add a main function to handle user input.
    # Aider chat conversation:
    
    # Context:
    USER: convert this into haskell code

--- crud.hs
+++ crud.hs
@@ -1,150 +1,96 @@
-#!/bin/python3
+import Database.SQLite.Simple
 
-import sqlite3
+create :: IO ()
+create = do
+  conn <- open "data.db"
+  execute_ conn "CREATE TABLE IF NOT EXISTS Users (ID INTEGER PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, AGE INTEGER NOT NULL, GENDER TEXT, SALARY INTEGER)"
+  putStrLn "Table created successfully"
 
-####################################################
-def create():
-	conn = sqlite3.connect('data.db')
-	print("Opened database successfully");
-	conn.execute('''
-		CREATE TABLE Users
-		(ID      INT   PRIMARY KEY  NOT NULL,
-		 NAME    TEXT               NOT NULL,
-		 AGE     INT                NOT NULL,
-		 GENDER  TEXT,
-		 SALARY  INT);
-	''')
-	print("Table created successfully");
+insert :: IO ()
+insert = do
+  conn <- open "data.db"
+  putStrLn "Enter ID: "
+  idd <- readLn
+  putStrLn "Enter Name: "
+  name <- getLine
+  putStrLn "Enter Age: "
+  age <- readLn
+  putStrLn "Enter your Gender: "
+  gender <- getLine
+  putStrLn "Enter your Salary: "
+  salary <- readLn
+  execute conn "INSERT INTO Users (ID, NAME, AGE, GENDER, SALARY) VALUES (?, ?, ?, ?, ?)" (idd, name, age, gender, salary)
+  putStrLn "Data Inserted Successfully"
 
-####################################################
-# FOR CREATING RECORDS FUNCTION DEFINITION
-def insert():
-#	try:
-		con = sqlite3.connect("data.db")
-		cursor = con.cursor()
-		while (True):
-			idd = int(input("Enter ID: "))
-			name = input("Enter Name: ")
-			age = int(input("Enter Age: "))
-			gender = input("Enter your Gender: ")
-			salary = int(input("Enter your Salary: "))
-			query = "INSERT into USERS(ID,NAME,AGE,GENDER,SALARY) VALUES (?,?,?,?,?);"
-			data = (idd,name,age,gender,salary)
-			cursor.execute(query, data)
-			con.commit()
-			if(cursor.execute(query,data)):
-				print("Data Inserted Successfully")
-			else:
-				print("Data not Inserted")
-#			ch = input("Do You want to Add More Records(Y/N): ")
-#			if ch == "N" or ch == "n":
-#				cursor.close()
-#				break
-#			else:
-#				pass
-#	except:
-#		print("Error in Record Creation\n")
+readOne :: IO ()
+readOne = do
+  conn <- open "data.db"
+  putStrLn "Enter Your ID: "
+  ids <- readLn
+  results <- query conn "SELECT * FROM Users WHERE ID = ?" (Only ids) :: IO [(Int, String, Int, Maybe String, Maybe Int)]
+  case results of
+    [] -> putStrLn "ID does not exist"
+    [(id, name, age, _, salary)] -> do
+      putStrLn $ "Name is: " ++ name
+      putStrLn $ "Age is: " ++ show age
+      putStrLn $ "Salary is: " ++ show salary
 
-####################################################
-# FOR READING ONE RECORD FUNCTION DEFINITION
-def read_one():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    ids = int(input("Enter Your ID: "))
-    query = "SELECT * from USERS WHERE id = ?"
-    result = cursor.execute(query, (ids,))
-    if (result):
-        for i in result:
-            print(f"Name is: {i[1]}")
-            print(f"Age is: {i[2]}")
-            print(f"Salary is: {i[4]}")
-    else:
-        print("Roll Number Does not Exist")
-        cursor.close()
+readAll :: IO ()
+readAll = do
+  conn <- open "data.db"
+  results <- query_ conn "SELECT * FROM Users" :: IO [(Int, String, Int, Maybe String, Maybe Int)]
+  putStrLn "<=== Available Records ===>"
+  mapM_ (\(id, name, age, _, salary) -> do
+    putStrLn $ "Name is: " ++ name
+    putStrLn $ "Age is: " ++ show age
+    putStrLn $ "Salary is: " ++ show salary
+    putStrLn "") results
 
-####################################################
-# FOR READING ALL RECORDS FUNCTION DEFINITION
-def read_all():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    query = "SELECT * from USERS"
-    result = cursor.execute(query)
-    if (result):
-        print("\n&lt;===Available Records===&gt;")
-        for i in result:
-            print(f"Name is : {i[1]}")
-            print(f"Age is : {i[2]}")
-            print(f"Salary is : {i[4]}\n")
-    else:
-        pass
-    
-####################################################
-# FOR UPDATING RECORDS FUNCTION DEFINITION
-def update():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    idd = int(input("Enter ID: "))
-    name = input("Enter Name: ")
-    age = int(input("Enter Age: "))
-    gender = input("Enter Gender: ")
-    salary = int(input("Enter Salary: "))
-    data = (name, age, gender, salary, idd,)
-    query = "UPDATE USERS set name = ?, age = ?, gender = ?, salary = ? WHERE id = ?"
-    result = cursor.execute(query, data)
-    con.commit()
-    cursor.close()
-    if (result):
-        print("Records Updated")
-    else:
-        print("Something Error in Updation")
+update :: IO ()
+update = do
+  conn <- open "data.db"
+  putStrLn "Enter ID: "
+  idd <- readLn
+  putStrLn "Enter Name: "
+  name <- getLine
+  putStrLn "Enter Age: "
+  age <- readLn
+  putStrLn "Enter Gender: "
+  gender <- getLine
+  putStrLn "Enter Salary: "
+  salary <- readLn
+  execute conn "UPDATE Users SET NAME = ?, AGE = ?, GENDER = ?, SALARY = ? WHERE ID = ?" (name, age, gender, salary, idd)
+  putStrLn "Records Updated"
 
-####################################################
-# FOR DELETING RECORDS FUNCTION DEFINITION
-def delete():
-    con = sqlite3.connect("data.db")
-    cursor = con.cursor()
-    idd = int(input("Enter ID: "))
-    query = "DELETE from USERS where ID = ?"
-    result = cursor.execute(query, (idd,))
-    con.commit()
-    cursor.close()
-    if (result):
-        print("One record Deleted")
-    else:
-        print("Something Error in Deletion")
+delete :: IO ()
+delete = do
+  conn <- open "data.db"
+  putStrLn "Enter ID: "
+  idd <- readLn
+  execute conn "DELETE FROM Users WHERE ID = ?" (Only idd)
+  putStrLn "One record Deleted"
 
-####################################################
-# MAIN BLOCK
-try:
-    while (True):
-        print("1). Insert Records: ")
-        print("2). Read Records: ")
-        print("3). Update Records: ")
-        print("4). Delete Records: ")
-        print("5). Exit")
-        ch = int(input("Enter Your Choice: "))
-        if (ch == 1):
-            insert()
-        elif (ch == 2):
-            print("1). Read Single Record")
-            print("2). Read All Records")
-            choice = int(input("Enter Your Choice: "))
-            if (choice == 1):
-                read_one()
-            elif (choice == 2):
-                read_all()
-            else:
-                print("Wrong Choice Entered")
-        elif (ch == 3):
-            update()
-        elif (ch == 4):
-            delete()
-        elif (ch == 5):
-            break
-        else:
-            print("Enter Correct Choice")
-except:
-    print("Database Error")
-
-####################################################
-#END
+main :: IO ()
+main = do
+  putStrLn "1). Insert Records: "
+  putStrLn "2). Read Records: "
+  putStrLn "3). Update Records: "
+  putStrLn "4). Delete Records: "
+  putStrLn "5). Exit"
+  putStrLn "Enter Your Choice: "
+  ch <- readLn
+  case ch of
+    1 -> insert
+    2 -> do
+      putStrLn "1). Read Single Record"
+      putStrLn "2). Read All Records"
+      putStrLn "Enter Your Choice: "
+      choice <- readLn
+      case choice of
+        1 -> readOne
+        2 -> readAll
+        _ -> putStrLn "Wrong Choice Entered"
+    3 -> update
+    4 -> delete
+    5 -> return ()
+    _ -> putStrLn "Enter Correct Choice"
